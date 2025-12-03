806581263fdef9561fb0124227d0e92d796c6f43
cathsuchurch <yanksone18@yahoo.com>
2025-12-03 20:58:13 +0000

    'change'

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,8 +1,8 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Maqete",
-  description : "Welcome to a world of unbeatable deals and exclusive offers! Experience lightning-fast deliveries and exceptional customer service designed for your convenience. Your satisfaction is our top priority, and we’re here to make your shopping experience seamless and enjoyable. Dive in and discover the difference!",
+  name: "Project",
+  description : "Clothing",
   keywords: "phones, mobile phones, smartphones, phones in ghana, affordable phones in ghana, budget friendly phones in Ghana, low budget phones in Ghana, best phones, best phones in Ghana, best online shop in ghana, best e-commerce in Ghana, best ecommerce in ghana, phone accessories, earbuds, phone covers, phone chargers, jerseys, men's fashion, women's fashion, online shopping, ecommerce, ghana, affordable, budget-friendly, clothing, apparel, shoes, bags, jewelry, electronics, gadgets, home decor, household items, ghana, ghanaian, accra, kumasi, online shopping ghana, e-commerce ghana, buy online in ghana, shop online ghana, cheap, discounts, deals, offers, sales, bargains, value for money, cost-effective, online shopping, e-commerce, internet shopping, fast delivery, reliable delivery, easy returns, hassle-free returns, secure payment, safe transactions, trusted, reputable, reliable, top brands, quality products, quality phones, customer reviews, testimonials, best prices, lowest prices, top-rated, high-rated, unique, exclusive, wide selection, diverse range, fashionable, trendy, stylish, convenient, time-saving, quality of life, lifestyle, happiness, satisfaction",
   navItems: [
     {
